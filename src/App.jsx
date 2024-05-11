99abc1324013d425d51586a966c8472427f39f50
rizkialimaulana <maulanarizkiali@gmail.com>
2024-05-12 03:15:08 +0700

    Slicing Brainwave landing page

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Benefits from "./components/Benefits";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+}
+
+export default App;
